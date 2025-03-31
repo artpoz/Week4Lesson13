4d1afc11ba49adbab85f688e7049bbc4e2c4c83b
Artur Poznański <apoznan5@gmail.com>
2025-03-31 16:09:39 +0200

    FizzBuzz implementation

--- Program.cs
+++ Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Week4Lesson13
 {
@@ -10,6 +6,47 @@ namespace Week4Lesson13
     {
         static void Main(string[] args)
         {
+            while (true)
+            {
+                var number = GetNumber();
+
+                if (number % 3 == 0 && number % 5 == 0)
+                {
+                    Console.WriteLine("FizzBuzz");
+                    continue;
+                }
+                if (number % 3 == 0)
+                {
+                    Console.WriteLine("Fizz");
+                    continue;
+                }
+                if (number % 5 == 0)
+                { 
+                    Console.WriteLine("Buzz");                 
+                    continue;
+                }
+
+                Console.WriteLine(number);
+            }           
+        }
+
+        public static int GetNumber()
+        {
+            while (true)
+            {
+                Console.WriteLine("Podaj liczbę (0 - koniec):");
+                if (!int.TryParse(Console.ReadLine(), out int number))
+                {
+                    Console.WriteLine("Podałeś nieprawidłowe dane. Spróbuj ponownie.");
+                    continue;
+                }
+
+                if (number == 0)
+                {
+                    Environment.Exit(0);
+                }
+                return number;
+            }
         }
     }
 }
